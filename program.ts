5a7aa77b1f3d1e6dbbe339ca698e079105e0d990
tsteelematc <tsteele@madisoncollege.edu>
2025-05-02 09:37:10 -0500

    With Effect docs, limped into running an Effect : - O

--- program.ts
+++ program.ts
@@ -1,9 +1,15 @@
 import { Effect } from 'effect';
 
-console.log("Hello Effect");
+console.log(
+    "Hello Effect"
+);
 
-// const myFirstEffect = Effect.succeed(42);
+const myFirstEffect = Effect.succeed(42);
 
-// Effect.sync(
-//     () => myFirstEffect
-// );
+const result = Effect.runSync(
+    myFirstEffect
+);
+
+console.log(
+    result
+)
