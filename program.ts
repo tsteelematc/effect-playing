8c1891378b18bf6a1ead4182d1eb7cd0357dd1c1
tsteelematc <tsteele@madisoncollege.edu>
2025-05-02 09:38:20 -0500

    All in on my formatting : - O

--- program.ts
+++ program.ts
@@ -1,10 +1,14 @@
-import { Effect } from 'effect';
+import { 
+    Effect 
+} from 'effect';
 
 console.log(
     "Hello Effect"
 );
 
-const myFirstEffect = Effect.succeed(42);
+const myFirstEffect = Effect.succeed(
+    42
+);
 
 const result = Effect.runSync(
     myFirstEffect
